fdcacc150867c9f3023c2790b5446be003d53c3e
Victor-als <victor.ccx814@gmail.com>
2024-06-04 16:54:45 -0300

    feat: add login and logout button

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,16 @@
+"use client"
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+//import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  // const {data} = useSession();
+  // const handlerLoginCard = async () => {
+  //   await signIn();
+  // }
   return ( 
     <Card>
       <CardContent className="px-5 py-8 justify-between items-center flex flex-row">
@@ -20,6 +27,15 @@ const Header = () => {
         >
           <MenuIcon />
         </Button>
+
+        {/* {data?.user 
+         ? <div>
+            <Button onClick={() => signOut()}>Logout</Button>
+            <h1>{data.user.name}</h1> 
+          </div> 
+         :  <Button onClick={handlerLoginCard}>Login</Button>
+        } */}
+       
       </CardContent>
     </Card>
    );
