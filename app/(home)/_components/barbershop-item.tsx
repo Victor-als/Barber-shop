93b70e5d672b2848020aa89d2989e520b68beafd
Victor-als <victor.ccx814@gmail.com>
2024-06-13 14:28:34 -0300

    update barbershop item top padding

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -21,7 +21,7 @@ const BarbershopItem = ({barbershop}: BarbershopItemProps) => {
 
   return ( 
     <Card className="min-w-full max-w-full rounded-2xl ">
-      <CardContent className="px-1 py-0">
+      <CardContent className="px-1 py-0 pt-1">
         <div className="w-full h-[159px] relative">
             <div className="absolute top-2 left-2 z-50">
               <Badge className="bg-[#221c3d] hover:bg-[#221c3] opacity-90 flex gap-1 items-center">
@@ -30,7 +30,7 @@ const BarbershopItem = ({barbershop}: BarbershopItemProps) => {
               </Badge>
             </div>
             <Image 
-                className="rounded-2xl" 
+                className=" rounded-2xl" 
                 src={barbershop.imageUrl}
                 sizes="100vw"
                 style={{
